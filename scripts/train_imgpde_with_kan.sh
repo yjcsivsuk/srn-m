9dee70f42f4e8de711e38b78fa599999552a73c6
yjcsivsuk <yjcsivsuk@gmail.com>
2024-06-10 04:25:45 +0800

    在图像数据提取过程中，如果不加sobel算子，效果会很不好

--- scripts/train_imgpde_with_kan.sh
+++ scripts/train_imgpde_with_kan.sh
@@ -3,11 +3,13 @@
 gpu=cpu
 seed=42
 
+pde_find_only_with_kan=False
 img_pde_find_with_kan=True
 pde_find_with_kan=False
+pde_find=False
 n_layers=2  # kan的层数
 # layers_hidden=[3，3，1]
-grid_size=10
+grid_size=3
 spline_order=3
 scale_noise=0.1
 scale_base=1.0
@@ -17,7 +19,7 @@ weight_decay=1e-5
 epoch=1000
 layer_idx=0
 lr=3e-3
-optim=Adam
+optim=AdamW
 
 out_dir=output/img_find-pde_with_kan/${n_layers}layers-li${layer_idx}-gs${grid_size}-wd${weight_decay}-opt${optim}-lr${lr}-ep${epoch}
 mkdir -p ${out_dir}
@@ -28,6 +30,8 @@ nohup python train_img_pde.py \
     --epoch ${epoch} \
     --n_layers ${n_layers} \
     --layer_idx ${layer_idx} \
+    --pde_find ${pde_find} \
+    --pde_find_only_with_kan ${pde_find_only_with_kan} \
     --img_pde_find_with_kan ${img_pde_find_with_kan} \
     --pde_find_with_kan ${pde_find_with_kan} \
     --grid_size ${grid_size} \
@@ -38,6 +42,7 @@ nohup python train_img_pde.py \
     --grid_eps ${grid_eps} \
     --weight_decay ${weight_decay} \
     --lr ${lr} \
+    --optim ${optim} \
     --out_dir ${out_dir} \
     > ${out_dir}/train.log 2>&1 & \
     echo $! > ${out_dir}/train.pid
